2f131c9c4c3df58242b59d9d5a493055ac723a1c
Teacher Radh <r.kamal@ph.goit.global>
2024-02-12 11:06:20 +0800

    Add discussion code

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -24,37 +24,38 @@
 
 // 10. End
 
-// // Described in documentation;
-// import SimpleLightbox from "simplelightbox";
-// // Additional styles import
+// Described in documentation;
+import SimpleLightbox from 'simplelightbox';
+// Additional styles import
 
-// import "simplelightbox/dist/simple-lightbox.min.css";
-// import { galleryItems } from './gallery-items.js';
-// // Change code below this line
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
 
-// // console.log(galleryItems);
-// const galleryList = document.querySelector(".gallery");
-// const createGallery = el => {
-//     return el.map(({ preview, original, description }) => {
-//     return `
-//     <li class="gallery__item">
-//       <a class="gallery__link" href="${original}">
-//         <img
-//           class="gallery__image"
-//           src="${preview}"
-//           data-source="${original}"
-//           alt="${description}"
-//         />
-//       </a>
-//     </li>`;
-//     })
-//     .join("");
-// };
-// const photosMarkup = createGallery(galleryItems);
-// galleryList.insertAdjacentHTML('beforeend', photosMarkup);
+// console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+const createGallery = el => {
+  return el
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>`;
+    })
+    .join('');
+};
+const photosMarkup = createGallery(galleryItems);
+galleryList.insertAdjacentHTML('beforeend', photosMarkup);
 
-// const lightbox = new SimpleLightbox('.gallery a', {
-//   captionsData: 'alt',
-//   captionDelay: 250,
-//   captionPosition: "bottom",
-// });
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,38 +2,36 @@
 // 2. Import the Vimeo Player API
 // 3. Import the throttle function from the lodash library
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
 // 4. Retrieve the iframe with the ID 'vimeo-player' and store it in iframe
+const iframe = document.querySelector('#vimeo-player');
+
 // 5. Instantiate a new Vimeo Player with the iframe
+const player = new Player(iframe);
 
 // 6. Define a function handleThrottle that takes data as a parameter
-//     6.1 Extract the current playback time in seconds from data
-//     6.2 Log the current playback time to the console
-//     6.3 Save the current playback time in local storage under the key 'videoplayer-current-time'
+const handleThrottle = function (data) {
+  //     6.1 Extract the current playback time in seconds from data
+  const timeInSeconds = data.seconds;
+  //     6.2 Log the current playback time to the console
+  console.log(timeInSeconds);
+  //     6.3 Save the current playback time in local storage under the key 'videoplayer-current-time'
+  localStorage.setItem('videoplayer-current-time', timeInSeconds);
+};
 
 // 7. Add an event listener to the player for the 'timeupdate' event
 //     7.1 Use the throttle function to limit calls to handleThrottle to once every 1000 milliseconds
 
+// player.on('timeupdate', handleThrottle);
+
+player.on('timeupdate', throttle(handleThrottle, 1000));
+
 // 8. Attempt to set the player's current playback time to the value retrieved from local storage under 'videoplayer-current-time'
 //     8.1 Note: The actual setting of the time is assumed to be handled internally by the player
 
-// 9. End
-
-// import Player from '@vimeo/player';
-// import throttle from 'lodash.throttle';
-
-// const iframe = document.querySelector('#vimeo-player');
-// const player = new Player(iframe);
-
-// const handleThrottle = function (data) {
-//   const timeInSeconds = data.seconds;
-//   console.log(timeInSeconds);
-//   localStorage.setItem('videoplayer-current-time', timeInSeconds);
-// };
-
-// player.on('timeupdate', throttle(handleThrottle, 1000));
-
-// player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
-
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
 // .then(function (seconds) {
 //   // seconds = the actual time that the player seeked to
 // })
@@ -48,3 +46,5 @@
 //       break;
 //   }
 // });
+
+// 9. End
